972e5f1fa3ed400209f09575368ae0931861ff2c
Provoski <provo.iyen27@gmail.com>
2023-01-22 07:55:08 -0800

    task 1 on sorting algorithm

---
+++ 0-bubble_sort.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include "sort.h"
+
+/**
+ * bubble_sort - sort array of integer in ascending order
+ * @array: array of integer to sort
+ * @size: size of the array
+ * Return: Nothing
+ */
+void bubble_sort(int *array, size_t size)
+{
+	size_t i;
+	size_t j;
+	int flag;
+	int temp;
+
+	temp = 0;
+	for (i = 0; i < (size - 1); i++)
+	{
+		flag = 0;
+		for (j = 0; j < (size -	1) - i; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				temp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = temp;
+				flag = 1;
+				print_array(array, size);
+			}
+		}
+		if (flag == 0)
+			break;
+	}
+}
